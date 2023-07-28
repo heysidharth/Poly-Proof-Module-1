e0e592d84f033bdfcd38f04bb1c93b9a9217a59c
Sidharth Samantaray <111923974+heysidharth@users.noreply.github.com>
2023-07-28 23:30:19 +0530

    Add files via upload

---
+++ metadata/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const nftAddress = "0xeeF5b06411134a7836Aa89cb5a29baF622F97E3B"
+  
---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "1",
+    description: "A footballer is scoring goal",
+    image: "QmNmV7bHHHEBZVkfGjXQ7ra5VHsCN49eHhyQPihEctGbjn",
+  },
+  {
+    name: "2",
+    description: "A footballer is scoring goal",
+    image: "QmPnYQrwLFyLNbHxNaiRev9jxQP7cwEXHHckYujWhMFg7A",
+  }, //change
+  {
+    name: "3",
+    description: "A footballer is scoring goal",
+    image: "QmRTosHyL7FzaVR3eKi9TtJNLYwtacCkbGYSoAMXzzNQpq",
+  },
+  {
+    name: "4",
+    description: "A footballer is scoring goal",
+    image: "QmW9CepgdjM3soL4KVa6ZgknSd3tPSyrnGurrj9hHLTDHR",
+  },
+  {
+    name: "5",
+    description: "A footballer is scoring goal.",
+    image: "QmXxVkiqte9sgGNNDePRDuqUPCJ6YK2VBSvjtcMZxBLJDW",
+  },
+];
+
+module.exports = nfts;
